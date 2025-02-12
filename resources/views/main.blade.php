cf76702d6c26c0a6c9c1bdf1dfa582c850eed8db
xelab04 <alexbissessur@gmail.com>
2025-02-12 23:32:42 +0400

    Fix footer

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -94,7 +94,7 @@
         <footer class="bg-gray-800 dark:bg-neutral-900 text-white py-6">
             <div class="container mx-auto text-center">
                 <p class="text-sm md:text-base">
-                    Made by Alex, who is [visibly] not a frontend dev.
+                    Made by the wider Mauritian tech community <3
                 </p>
             </div>
         </footer>
