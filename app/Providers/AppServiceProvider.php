98504ff31f4e437ca3b17ab849760df1d17e2a22
Alex Bissessur <61658620+xelab04@users.noreply.github.com>
2025-04-21 12:51:51 +0400

    remove force https test
    we test in prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceHttps();
+        // URL::forceHttps();
         Model::unguard();
     }
 }
