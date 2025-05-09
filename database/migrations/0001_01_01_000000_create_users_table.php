3c89d64b9ce90b0a80e372a92981c503d39c8b3e
alex <alexbissessur@gmail.com>
2025-05-09 08:47:40 +0400

    fix: remove duplicate table fields in user migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,12 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('firstname')->nullable();
-            $table->string('lastname')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('admin')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
